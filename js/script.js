14c7e02826f5d000c41f553ba1487489c7d39355
Adrian Herbert <aherbert2020@bklawtech.com>
2018-01-10 23:27:08 +0000

    I have done more of the work.

--- js/script.js
+++ js/script.js
@@ -5,23 +5,31 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
+
 $( document ).ready(function() {
   
+	$("#submit").click(function(){
 
-
-
+	});
+	
 });
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
-
+function pigLatin(word) {
+	return word + "ay"
+};
 
 
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
+	//It should return a transfromed sentanc
+function sentenceToPigLatin(sentence){
+	var words = sentence.split(' ');
 
-
+	return words; 
+}
+console.log(sentenceToPigLatin("I am here."));
